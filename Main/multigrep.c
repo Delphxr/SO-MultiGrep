17b4cbfef48ec0196e48c1310ed825b090f4d579
Oswaldo <lorf025@gmail.com>
2022-09-23 00:11:23 -0600

    runner y mejoras varias

--- Main/multigrep.c
+++ Main/multigrep.c
@@ -1,16 +1,28 @@
 /*
 - Proyecto 1
 - Oswaldo Ramírez - 2020044182
-*
 */
 
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define REGEXSZ 128
-#define LINESZ 1024
+#define REGEXSZ 128 //tamanno maximo de un regex
+#define LINESZ 1024  //tamanno maximo de una linea en un archivo
+#define NUM_THREADS 5 //numero de hilos en los que va a corrrer el programa
+
+void menu(){
+    printf("+---------------------------------------------+\n");
+    printf("|  __  __       _ _   _  ____                 |\n");
+    printf("| |  \\/  |_   _| | |_(_)/ ___|_ __ ___ _ __   |\n");
+    printf("| | |\\/| | | | | | __| | |  _| '__/ _ \\ '_ \\  |\n");
+    printf("| | |  | | |_| | | |_| | |_| | | |  __/ |_) | |\n");
+    printf("| |_|  |_|\\__,_|_|\\__|_|\\____|_|  \\___| .__/  |\n");
+    printf("|                                     |_|     |\n");
+    printf("+---------------------------------------------+\n");
+}
 
 // recibe el nombre de un archivo y un regex para ejecutarlo
 int regex_file(char filename[], regex_t regex) {
@@ -22,12 +34,11 @@ int regex_file(char filename[], regex_t regex) {
 
     if (file_input == NULL) {
         printf("! No se pudo abrir el archivo: %s correctamente, compruebe que exista o que hayan los permisos necesarios\n", filename);
-        fclose(file_input);
         return -1;
     }
 
     while (fgets(line, LINESZ, file_input) != NULL) {
-        if(!regexec(&regex, line, 0, NULL, 0)){ //se da el caso
+        if(!regexec(&regex, line, 0, NULL, 0)){ //se da el caso del regex
 
             //por cuestiones de estetica recortamos el string si es muy largo
             if (strlen(line) > 50)
@@ -35,11 +46,9 @@ int regex_file(char filename[], regex_t regex) {
             else
                 printf("%s\t%d\t%s",filename,current_line,line);
         }
-        //printf("%s",line);
+
         current_line++;
     }
-
-    //printf("+ Backup para %s creado correctamente! \n", filename);
 
     fclose(file_input);
     printf("\n");
@@ -49,7 +58,12 @@ int regex_file(char filename[], regex_t regex) {
 int main(int argc, char *argv[]) {
     int i;
     regex_t regex;
-    char regex_s[REGEXSZ];
+    char regex_s[REGEXSZ]; //aqui vamos a guardar el regex
+    clock_t start, end; // hora de inicio y finalizacion deel programa
+    double execution_time; // para tener el tiempo de ejecucion del programa
+    
+    menu();
+    start = clock();
 
     // revisamos si hay mas de 2 argumentos (un regex y un archivo como minimo), el argumento 0 es el nombre del programa
     if (argc >= 3) {
@@ -61,12 +75,17 @@ int main(int argc, char *argv[]) {
             exit(1);
         }
 
+        //aqui hay que meter los hilos de ejecucion
         for (int i = 2; i < argc; i++) {
             regex_file(argv[i],regex);
         }
 
     } else
         printf("Se espera que haya al menos dos argumentos.\n");
+
+    end = clock();
+    execution_time = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("Tiempo de ejecución en segundos : %f \n", execution_time);
 
     exit(0);
 }
---
+++ Main/run.sh
@@ -0,0 +1,2 @@
+clear
+./multigrep "espada" quijote_1.txt quijote_2.txt quijote_3.txt quijote_4.txt quijote_5.txt quijote_6.txt quijote_7.txt quijote_8.txt quijote_9.txt quijote_10.txt quijote_11.txt quijote_12.txt
